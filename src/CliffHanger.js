831b718408a4c6279e9d6bf36c3e0be93fcf8abf
Winston <winston@nitidbit.com>
2020-04-11 23:52:17 -0700

    Credit on last page.

--- src/CliffHanger.js
+++ src/CliffHanger.js
@@ -8,15 +8,18 @@ export function CliffHanger(props) {
       <p>
         To be continued...
       </p>
+      <br/>
+      <p>
+        A game by Winston Wolff and Jay Hirsch. Testing by Walter Wolff-Tang.
+      </p>
       <p>
         If you have enjoyed this puzzle hunt so far, and want to see the end of
-        it, please send me an email. Just one sentence is enough. It takes effort to
-        craft these puzzles and I don't want to make it if nobody's playing.
+        it, please send us an email. Just one sentence is enough. It takes effort to
+        craft these puzzles and we don't want to make it if nobody's playing.
       </p>
       <p>
         Send an email to: {' '}
         <a href="mailto:winstonw@stratolab.com">winstonw@stratolab.com</a>.
-        
       </p>
     </div>
   );
